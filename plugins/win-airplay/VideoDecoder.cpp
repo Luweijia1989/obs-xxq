8ea651b6fb61171ef5f10d4174c0d2ae2312df9d
weijialu <120215034@qq.com>
2020-10-21 16:12:45 +0800

    切换成软解 硬解兼容性有问题

--- plugins/win-airplay/VideoDecoder.cpp
+++ plugins/win-airplay/VideoDecoder.cpp
@@ -2,131 +2,20 @@
 #include "airplay-server.h"
 #include <obs.hpp>
 
-static AVBufferRef *hw_device_ctx = NULL;
-static enum AVPixelFormat hw_pix_fmt = AV_PIX_FMT_DXVA2_VLD;
-static FILE *output_file = NULL;
-
-static int hw_decoder_init(AVCodecContext *ctx, const enum AVHWDeviceType type)
-{
-	int err = 0;
-
-	if ((err = av_hwdevice_ctx_create(&hw_device_ctx, type, NULL, NULL,
-					  0)) < 0) {
-		fprintf(stderr, "Failed to create specified HW device.\n");
-		return err;
-	}
-	ctx->hw_device_ctx = av_buffer_ref(hw_device_ctx);
-
-	return err;
-}
-
-static enum AVPixelFormat get_hw_format(AVCodecContext *ctx,
-					const enum AVPixelFormat *pix_fmts)
-{
-	const enum AVPixelFormat *p;
-
-	for (p = pix_fmts; *p != -1; p++) {
-		if (*p == hw_pix_fmt)
-			return *p;
-	}
-
-	fprintf(stderr, "Failed to get HW surface format.\n");
-	return AV_PIX_FMT_NONE;
-}
-
-static int decode_write(AVCodecContext *avctx, AVPacket *packet)
-{
-	AVFrame *frame = NULL, *sw_frame = NULL;
-	AVFrame *tmp_frame = NULL;
-	uint8_t *buffer = NULL;
-	int size;
-	int ret = 0;
-
-	ret = avcodec_send_packet(avctx, packet);
-	if (ret < 0) {
-		fprintf(stderr, "Error during decoding\n");
-		return ret;
-	}
-
-	while (1) {
-		if (!(frame = av_frame_alloc()) ||
-		    !(sw_frame = av_frame_alloc())) {
-			fprintf(stderr, "Can not alloc frame\n");
-			ret = AVERROR(ENOMEM);
-			goto fail;
-		}
-
-		ret = avcodec_receive_frame(avctx, frame);
-		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-			av_frame_free(&frame);
-			av_frame_free(&sw_frame);
-			return 0;
-		} else if (ret < 0) {
-			fprintf(stderr, "Error while decoding\n");
-			goto fail;
-		}
-
-		if (frame->format == hw_pix_fmt) {
-			/* retrieve data from GPU to CPU */
-			if ((ret = av_hwframe_transfer_data(sw_frame, frame,
-							    0)) < 0) {
-				fprintf(stderr,
-					"Error transferring the data to system memory\n");
-				goto fail;
-			}
-			tmp_frame = sw_frame;
-		} else
-			tmp_frame = frame;
-
-		size = av_image_get_buffer_size(
-			(AVPixelFormat)tmp_frame->format, tmp_frame->width,
-			tmp_frame->height, 1);
-		buffer = (uint8_t *)av_malloc(size);
-		if (!buffer) {
-			fprintf(stderr, "Can not alloc buffer\n");
-			ret = AVERROR(ENOMEM);
-			goto fail;
-		}
-		ret = av_image_copy_to_buffer(
-			buffer, size, (const uint8_t *const *)tmp_frame->data,
-			(const int *)tmp_frame->linesize,
-			(AVPixelFormat)tmp_frame->format, tmp_frame->width,
-			tmp_frame->height, 1);
-		if (ret < 0) {
-			fprintf(stderr, "Can not copy image to buffer\n");
-			goto fail;
-		}
-
-		if ((ret = fwrite(buffer, 1, size, output_file)) < 0) {
-			fprintf(stderr, "Failed to dump raw data.\n");
-			goto fail;
-		}
-
-	fail:
-		av_frame_free(&frame);
-		av_frame_free(&sw_frame);
-		av_freep(&buffer);
-		if (ret < 0)
-			return ret;
-	}
-}
-
-VideoDecoder::VideoDecoder(ScreenMirrorServer *s)
-	: m_server(s)
-{
-	
-}
+VideoDecoder::VideoDecoder(ScreenMirrorServer *s) : m_server(s) {}
 
 VideoDecoder::~VideoDecoder()
 {
 	uninitFFMPEG();
 }
 
-void VideoDecoder::docode(uint8_t *data, size_t data_len, bool is_key, uint64_t ts)
+void VideoDecoder::docode(uint8_t *data, size_t data_len, bool is_key,
+			  uint64_t ts)
 {
 	int canOutput = -1;
 	if (is_key) {
-		if (m_pCodecCtx && data_len == m_pCodecCtx->extradata_size && memcmp(data, m_pCodecCtx->extradata, data_len) == 0)
+		if (m_pCodecCtx && data_len == m_pCodecCtx->extradata_size &&
+		    memcmp(data, m_pCodecCtx->extradata, data_len) == 0)
 			return;
 		uninitFFMPEG();
 
@@ -137,34 +26,27 @@ void VideoDecoder::docode(uint8_t *data, size_t data_len, bool is_key, uint64_t 
 	} else {
 		if (m_bCodecOpened) {
 			AVPacket pkt1, *packet = &pkt1;
-			AVFrame *tmp_frame = NULL;
+			int frameFinished;
+			AVFrame *pFrame;
+
+			pFrame = av_frame_alloc();
 
 			av_new_packet(packet, data_len);
 			memcpy(packet->data, data, data_len);
+
 			int ret =
 				avcodec_send_packet(this->m_pCodecCtx, packet);
-			if (ret == 0)
-			{
-				while (true)
-				{
-					AVFrame *pFrame = av_frame_alloc();
-					AVFrame *sw_frame = av_frame_alloc();
-					ret = avcodec_receive_frame(m_pCodecCtx, pFrame);
-					if (ret == 0) {
-						av_hwframe_transfer_data(sw_frame, pFrame, 0);
-						sw_frame->pts = ts;
-						m_server->outputVideo(sw_frame);
-						av_frame_free(&pFrame);
-					}
-					else
-					{
-						av_frame_free(&pFrame);
-						av_frame_free(&sw_frame);
-						break;
-					}
-				}
-			}
+			frameFinished = avcodec_receive_frame(this->m_pCodecCtx,
+							      pFrame);
+
 			av_packet_unref(packet);
+
+			// Did we get a video frame?
+			if (frameFinished == 0) {
+				pFrame->pts = ts;
+				m_server->outputVideo(pFrame);
+			} else
+				av_frame_free(&pFrame);
 		}
 	}
 }
@@ -175,31 +57,18 @@ int VideoDecoder::initFFMPEG(const void *privatedata, int privatedatalen)
 		m_pCodec = avcodec_find_decoder(AV_CODEC_ID_H264);
 		m_pCodecCtx = avcodec_alloc_context3(m_pCodec);
 	}
-
-	AVHWDeviceType type = av_hwdevice_find_type_by_name("dxva2");
-	if (type == AV_HWDEVICE_TYPE_NONE) {
-		fprintf(stderr, "Device type %s is not supported.\n", "dxva2");
-		fprintf(stderr, "Available device types:");
-		while ((type = av_hwdevice_iterate_types(type)) !=
-		       AV_HWDEVICE_TYPE_NONE)
-			fprintf(stderr, " %s", av_hwdevice_get_type_name(type));
-		fprintf(stderr, "\n");
+	if (m_pCodec == NULL) {
 		return -1;
 	}
 
-	if (!(m_pCodecCtx = avcodec_alloc_context3(m_pCodec)))
-		return AVERROR(ENOMEM);
-
-	m_pCodecCtx->get_format = get_hw_format;
 	m_pCodecCtx->extradata = (uint8_t *)av_malloc(privatedatalen);
 	m_pCodecCtx->extradata_size = privatedatalen;
 	memcpy(m_pCodecCtx->extradata, privatedata, privatedatalen);
+	m_pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+	int res = avcodec_open2(m_pCodecCtx, m_pCodec, NULL);
+	if (res < 0) {
 
-	if (hw_decoder_init(m_pCodecCtx, type) < 0)
-		return -1;
-
-	int ret = -1;
-	if ((ret = avcodec_open2(m_pCodecCtx, m_pCodec, NULL)) < 0) {
+		printf("Failed to initialize decoder\n");
 		return -1;
 	}
 
@@ -215,10 +84,8 @@ void VideoDecoder::uninitFFMPEG()
 			av_freep(&m_pCodecCtx->extradata);
 		}
 		avcodec_free_context(&m_pCodecCtx);
-		av_buffer_unref(&hw_device_ctx);
 		m_pCodecCtx = NULL;
 		m_pCodec = NULL;
-		hw_device_ctx = NULL;
 	}
 	if (m_pSwsCtx) {
 		sws_freeContext(m_pSwsCtx);
