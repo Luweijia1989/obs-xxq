4a706af802a2a3b31e237cadc997a2dbcdceea59
luweijia <luweijia@bixin.cn>
2021-08-09 16:26:32 +0800

    Bonjour服务检测安装

--- plugins/win-airplay/airplay2/main.cpp
+++ plugins/win-airplay/airplay2/main.cpp
@@ -20,19 +20,25 @@ static BOOL Is64BitWindows()
 #endif
 }
 
-static void bonjourCheckInstall()
+
+static void installBonjourService()
 {
 	BOOL is64 = Is64BitWindows();
-	TCHAR path[MAX_PATH] ={0};
+	TCHAR path[MAX_PATH] = {0};
 	DWORD len = GetTempPath(MAX_PATH, path);
-	_tcscpy_s(path+len, MAX_PATH-len, is64 ? L"Bonjour64.msi" : L"Bonjour.msi");
+	_tcscpy_s(path + len, MAX_PATH - len,
+		  is64 ? L"Bonjour64.msi" : L"Bonjour.msi");
 
-	if (!PathFileExists(path))
-	{
-		HANDLE hFile = CreateFile(path, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-		if (hFile != INVALID_HANDLE_VALUE)
-		{
-			HRSRC res = FindResource(NULL, MAKEINTRESOURCE(is64 ? IDR_SETUPFILE2 : IDR_SETUPFILE1), RT_RCDATA);
+	if (!PathFileExists(path)) {
+		HANDLE hFile = CreateFile(path, GENERIC_WRITE, FILE_SHARE_READ,
+					  NULL, CREATE_ALWAYS,
+					  FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hFile != INVALID_HANDLE_VALUE) {
+			HRSRC res = FindResource(
+				NULL,
+				MAKEINTRESOURCE(is64 ? IDR_SETUPFILE2
+						     : IDR_SETUPFILE1),
+				RT_RCDATA);
 			HGLOBAL res_handle = LoadResource(NULL, res);
 			auto res_data = LockResource(res_handle);
 			auto res_size = SizeofResource(NULL, res);
@@ -78,22 +84,135 @@ static void bonjourCheckInstall()
 	CloseHandle(pi.hThread);
 }
 
+
+static void bonjourCheckInstall()
+{
+	DWORD dwOldCheckPoint;
+	DWORD dwStartTickCount;
+	DWORD dwWaitTime;
+	SC_HANDLE schSCManager = OpenSCManager(
+						NULL,                    // local computer
+						NULL,                    // servicesActive database 
+						SC_MANAGER_ALL_ACCESS);  // full access rights 
+ 
+	if (NULL == schSCManager) 
+	{
+		printf("OpenSCManager failed (%d)\n", GetLastError());
+		return;
+	}
+
+	SC_HANDLE schService = OpenService(schSCManager,        // SCM database
+				 L"Bonjour Service",           // name of service
+				 SERVICE_ALL_ACCESS); // full access
+
+	if (schService == NULL) {
+		installBonjourService();
+
+		schService = OpenService(schSCManager,        // SCM database
+					 L"Bonjour Service",  // name of service
+					 SERVICE_ALL_ACCESS); // full access
+		if (schService == NULL)
+			goto END;
+	}
+
+	SERVICE_STATUS_PROCESS ssStatus;
+	DWORD dwBytesNeeded;
+	if (!QueryServiceStatusEx(
+		    schService,                     // handle to service
+		    SC_STATUS_PROCESS_INFO,         // information level
+		    (LPBYTE)&ssStatus,              // address of structure
+		    sizeof(SERVICE_STATUS_PROCESS), // size of structure
+		    &dwBytesNeeded)) // size needed if buffer is too small
+		goto END;
+
+	if (ssStatus.dwCurrentState == SERVICE_STOPPED) {
+		if (!StartService(
+			schService,  // handle to service 
+			0,           // number of arguments 
+			NULL) )      // no arguments 
+			goto END;
+
+			// Check the status until the service is no longer start pending. 
+ 
+		if (!QueryServiceStatusEx( 
+			schService,                     // handle to service 
+			SC_STATUS_PROCESS_INFO,         // info level
+			(LPBYTE) &ssStatus,             // address of structure
+			sizeof(SERVICE_STATUS_PROCESS), // size of structure
+			&dwBytesNeeded ) )              // if buffer too small
+			goto END;
+ 
+		// Save the tick count and initial checkpoint.
+
+		dwStartTickCount = GetTickCount();
+		dwOldCheckPoint = ssStatus.dwCheckPoint;
+
+		while (ssStatus.dwCurrentState == SERVICE_START_PENDING) 
+		{ 
+			// Do not wait longer than the wait hint. A good interval is 
+			// one-tenth the wait hint, but no less than 1 second and no 
+			// more than 10 seconds. 
+ 
+			dwWaitTime = ssStatus.dwWaitHint / 10;
+
+			if( dwWaitTime < 1000 )
+				dwWaitTime = 1000;
+			else if ( dwWaitTime > 10000 )
+				dwWaitTime = 10000;
+
+			Sleep( dwWaitTime );
+
+			// Check the status again. 
+ 
+			if (!QueryServiceStatusEx( 
+						schService,             // handle to service 
+						SC_STATUS_PROCESS_INFO, // info level
+						(LPBYTE) &ssStatus,             // address of structure
+						sizeof(SERVICE_STATUS_PROCESS), // size of structure
+						&dwBytesNeeded ) )              // if buffer too small
+			{
+				printf("QueryServiceStatusEx failed (%d)\n", GetLastError());
+				break; 
+			}
+ 
+			if ( ssStatus.dwCheckPoint > dwOldCheckPoint )
+			{
+				// Continue to wait and check.
+
+				dwStartTickCount = GetTickCount();
+				dwOldCheckPoint = ssStatus.dwCheckPoint;
+			}
+			else
+			{
+				if(GetTickCount()-dwStartTickCount > ssStatus.dwWaitHint)
+				{
+					// No progress made within the wait hint.
+					break;
+				}
+			}
+		} 
+	}
+
+END:
+	if (schService)
+		CloseServiceHandle(schService);
+	if (schSCManager)
+		CloseServiceHandle(schSCManager);
+}
+
 int main(int argv, char *argc[])
 {
 	SetErrorMode(SEM_FAILCRITICALERRORS);
 	_setmode(_fileno(stdin), O_BINARY);
 	freopen("NUL", "w", stderr);
+
+	bonjourCheckInstall();
 	 
 	CAirServer server;
 	server.outputStatus(MIRROR_STOP);
 	if (!server.start()) {
-		bonjourCheckInstall();
-		if (!server.start())
-		{
-			server.outputStatus(MIRROR_ERROR);
-			return -1;
-		}
-		
+		server.outputStatus(MIRROR_ERROR);
+		return -1;
 	}
 
 	uint8_t buf[1024] = {0};
