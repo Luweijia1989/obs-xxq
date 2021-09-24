6c19ac111a2baf843dc88005953ef15bf3b60752
fengruiquan <fengruiquan@bixin.cn>
2021-09-24 15:53:22 +0800

    更新手机投屏提示

--- plugins/win-airplay/airplay-server.cpp
+++ plugins/win-airplay/airplay-server.cpp
@@ -125,12 +125,12 @@ void ScreenMirrorServer::dumpResourceImgs()
 	DWORD len = GetTempPathA(MAX_PATH, (char *)prefix.data());
 	prefix.resize(len);
 	m_resourceImgs.push_back(prefix + "pic_mirror_connecting.gif");
-	m_resourceImgs.push_back(prefix + "pic_android_cableprojection2.png");
-	m_resourceImgs.push_back(prefix + "pic_android_screencastfailed_cableprojection.png");
-	m_resourceImgs.push_back(prefix + "pic_ios_cableprojection.png");
-	m_resourceImgs.push_back(prefix + "pic_ios_screencastfailed_cableprojection.png");
-	m_resourceImgs.push_back(prefix + "pic_ios_wirelessprojection.png");
-	m_resourceImgs.push_back(prefix + "pic_ios_screencastfailed_wirelessprojection.png");
+	m_resourceImgs.push_back(prefix + "pic_android_cableprojection_1.png");
+	m_resourceImgs.push_back(prefix + "pic_android_screencastfailed_cableprojection_1.png");
+	m_resourceImgs.push_back(prefix + "pic_ios_cableprojection_1.png");
+	m_resourceImgs.push_back(prefix + "pic_ios_screencastfailed_cableprojection_1.png");
+	m_resourceImgs.push_back(prefix + "pic_ios_wirelessprojection_1.png");
+	m_resourceImgs.push_back(prefix + "pic_ios_screencastfailed_wirelessprojection_1.png");
 
 	std::vector<int> ids = { IDB_PNG1, IDB_PNG2, IDB_PNG3, IDB_PNG4, IDB_PNG5, IDB_PNG6 };
 
