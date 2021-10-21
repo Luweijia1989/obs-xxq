5e969d735542edfaaaad0164b4f45ae861f04283
luweijia <luweijia@bixin.cn>
2021-10-21 18:58:00 +0800

    fix投屏资源

--- plugins/win-airplay/resource.h
+++ plugins/win-airplay/resource.h
@@ -2,21 +2,21 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDB_PNG1                        118
-#define IDB_PNG2                        119
-#define IDB_PNG3                        120
-#define IDB_PNG4                        121
-#define IDB_PNG5                        122
-#define IDB_PNG6                        123
 #define IDB_BITMAP1                     124
-#define IDB_PNG7                        127
-#define IDB_PNG8                        128
+#define IDB_PNG1                        137
+#define IDB_PNG2                        138
+#define IDB_PNG3                        139
+#define IDB_PNG4                        140
+#define IDB_PNG5                        141
+#define IDB_PNG6                        142
+#define IDB_PNG7                        143
+#define IDB_PNG8                        144
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        145
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
