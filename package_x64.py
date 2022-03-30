c0e8d7483afe6ef676bfa4656446156749380e94
luweijia1989 <120215034@qq.com>
2022-03-30 10:06:53 +0800

    32 and 64打包脚本

---
+++ package_x64.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os
+import sys
+import time
+import platform
+import shutil
+import glob
+import json
+import zipfile
+import requests
+  
+def system(command):
+    retcode = os.system(command)
+    if retcode != 0:
+        raise Exception("Error while executing:\n\t %s" % command)
+def main():
+    version = "0.0.0"
+    isOnline = "0"
+    channel = "stable"
+    paramlen = len(sys.argv)
+    if paramlen == 3:
+       version = sys.argv[1]
+       isOnline = sys.argv[2]
+       print("version:"+str(version)+" isOnline:"+str(isOnline))
+    elif paramlen == 2:
+       version = sys.argv[1]
+       print("version:"+str(version))
+    elif paramlen == 1:
+       print("version:"+str(version)+" isOnline:"+str(isOnline))
+    else:
+       print("params error.");
+       return;
+    if isOnline == "0":
+       channel = "test"
+    system('conan export-pkg . obs-xxq_x64/%s@bixin/%s -s compiler.version=15 -s arch=x86_64 -s build_type=Debug --force' % (version,channel))
+    system('conan export-pkg . obs-xxq_x64/%s@bixin/%s -s compiler.version=15 -s arch=x86_64 -s build_type=Release --force' % (version,channel))
+    if isOnline == "1":
+       system('conan upload obs-xxq_x64/%s@bixin/%s --all -r=pc' % (version,channel))
+       system('git tag %s' % version)
+       system('git push --tags')
+if __name__ == "__main__":
+    main()
