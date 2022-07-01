e68b35ec460df24fd4e29a21f0d043d29abe77d0
luweijia <luweijia@bixin.cn>
2022-07-01 14:39:41 +0800

    fix: 移除因imgui引入的隐式加载d3dcompiler.dll。修复某些系统缺失此dll导致的load失败，进而捕获游戏失败。

--- plugins/win-capture/graphics-hook/third-party/imgui/imgui_impl_dx12.cpp
+++ plugins/win-capture/graphics-hook/third-party/imgui/imgui_impl_dx12.cpp
@@ -40,9 +40,6 @@
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <d3dcompiler.h>
-#ifdef _MSC_VER
-#pragma comment(lib, "d3dcompiler") // Automatically link with d3dcompiler.lib as we are using D3DCompile() below.
-#endif
 
 // DirectX data
 static ID3D12Device*                g_pd3dDevice = NULL;
