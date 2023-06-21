d7af6863a5d95cec992ec680a18e46d6a9aa93a2
马瑞 <marui@yupaopao.cn>
2023-06-21 15:25:55 +0800

    webcapture优化+cmakelist修改

--- plugins/obs-qmlview/webcapture.cpp
+++ plugins/obs-qmlview/webcapture.cpp
@@ -1,10 +1,7 @@
 #include "webcapture.h"
 #include <QDebug>
 
-WebCapture::WebCapture(QString memoryName)
-	: m_source(nullptr), m_imageTexture(nullptr), width(0), height(0)
-{
-}
+WebCapture::WebCapture(QString memoryName) : m_source(nullptr), m_imageTexture(nullptr), width(0), height(0) {}
 
 WebCapture::~WebCapture()
 {
@@ -23,8 +20,7 @@ void WebCapture::createTexture(int w, int h)
 	width = w;
 	height = h;
 	obs_enter_graphics();
-	m_imageTexture =
-		gs_texture_create(width, height, GS_BGRA, 1, NULL, GS_DYNAMIC);
+	m_imageTexture = gs_texture_create(width, height, GS_BGRA, 1, NULL, GS_DYNAMIC);
 	obs_leave_graphics();
 	textureCreated = true;
 }
@@ -35,7 +31,7 @@ void WebCapture::updateTextureData(obs_data_t *command)
 	uint8_t *data = (uint8_t *)obs_data_get_int(command, "imageData");
 	int w = obs_data_get_int(command, "width");
 	int h = obs_data_get_int(command, "height");
-	if (w != width || h != height) {
+	if (w != width || h != height || !obs_data_has_user_value(command, "hScale") || !obs_data_has_user_value(command, "wScale")) {
 		struct obs_video_info ovi;
 		obs_get_video_info(&ovi);
 
@@ -50,15 +46,13 @@ void WebCapture::updateTextureData(obs_data_t *command)
 			if (gameRatio > 0.5f) {
 				tw = width > ovi.base_width ? ovi.base_width : width;
 				th = tw / gameRatio;
-			}
-			else
-			{
+			} else {
 				th = ovi.base_height;
 				tw = gameRatio * ovi.base_height;
-			}			
+			}
 		}
 
-		obs_data_t *setting = obs_source_get_settings(m_source);	
+		obs_data_t *setting = obs_source_get_settings(m_source);
 		obs_data_set_double(setting, "wScale", (double)tw / (double)w);
 		obs_data_set_double(setting, "hScale", (double)th / (double)h);
 		obs_data_release(setting);
@@ -81,11 +75,9 @@ static const char *webcapture_source_get_name(void *unused)
 
 static void webcapture_source_update(void *data, obs_data_t *settings) {}
 
-static void *webcapture_source_create(obs_data_t *settings,
-				      obs_source_t *source)
+static void *webcapture_source_create(obs_data_t *settings, obs_source_t *source)
 {
-	WebCapture *capture =
-		new WebCapture(obs_data_get_string(settings, "memroyName"));
+	WebCapture *capture = new WebCapture(obs_data_get_string(settings, "memroyName"));
 	capture->m_source = source;
 	return capture;
 }
@@ -135,8 +127,7 @@ static void webcapture_source_render(void *data, gs_effect_t *effect)
 		return;
 	}
 
-	gs_effect_set_texture(gs_effect_get_param_by_name(effect, "image"),
-			      capture->m_imageTexture);
+	gs_effect_set_texture(gs_effect_get_param_by_name(effect, "image"), capture->m_imageTexture);
 	obs_source_draw(capture->m_imageTexture, 0, 0, 0, 0, false);
 }
 
@@ -154,42 +145,41 @@ static void webcapture_command(void *data, obs_data_t *command)
 	}
 }
 
-struct obs_source_info webcapture_source_info = {
-	"webcapture_source",
-	OBS_SOURCE_TYPE_INPUT,
-	OBS_SOURCE_VIDEO | OBS_SOURCE_DO_NOT_DUPLICATE,
-	webcapture_source_get_name,
-	webcapture_source_create,
-	webcapture_source_destroy,
-	webcapture_source_getwidth,
-	webcapture_source_getheight,
-	webcapture_source_defaults,
-	webcapture_source_properties,
-	webcapture_source_update,
-	nullptr,
-	nullptr,
-	webcapture_source_show,
-	webcapture_source_hide,
-	nullptr,
-	webcapture_source_render,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	nullptr,
-	webcapture_command};
+struct obs_source_info webcapture_source_info = {"webcapture_source",
+						 OBS_SOURCE_TYPE_INPUT,
+						 OBS_SOURCE_VIDEO | OBS_SOURCE_DO_NOT_DUPLICATE,
+						 webcapture_source_get_name,
+						 webcapture_source_create,
+						 webcapture_source_destroy,
+						 webcapture_source_getwidth,
+						 webcapture_source_getheight,
+						 webcapture_source_defaults,
+						 webcapture_source_properties,
+						 webcapture_source_update,
+						 nullptr,
+						 nullptr,
+						 webcapture_source_show,
+						 webcapture_source_hide,
+						 nullptr,
+						 webcapture_source_render,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 nullptr,
+						 webcapture_command};
